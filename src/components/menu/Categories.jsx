a22619f166000fe1b7712b37417dcfe1541e3887
Abdul-Rehman636 <rehmankk636@gmail.com>
2025-08-20 20:43:31 +0500

    Adding json for categories.

--- src/components/menu/Categories.jsx
+++ src/components/menu/Categories.jsx
@@ -3,12 +3,10 @@ const Categories = () => {
     <div className="lg:w-[94%] font-poppins">
       <div className="flex justify-between items-center">
         <p className="text-[25px] font-medium text-white">Categories</p>
-        <button className="text-[16px] font-medium text-[#333333] py-[14px] px-[22px] bg-[#FAC1D9] rounded-[7.5px]">
+        <button className="text-[16px] font-medium text-[#333333] py-[14px] px-[22px] bg-[#FAC1D9] rounded-[7.5px] cursor-pointer">
           Add New Category
         </button>
       </div>
-
-      
     </div>
   );
 };
